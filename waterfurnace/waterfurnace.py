7a05ad7dde8891c4fbd81fa90cf380e3a61fea0c
Sean Dague <sean@dague.net>
2025-09-13 08:12:28 -0400

    update to allow connections
    # Conflicts:
    #	waterfurnace/waterfurnace.py

--- waterfurnace/waterfurnace.py
+++ waterfurnace/waterfurnace.py
@@ -7,6 +7,7 @@ import json
 import threading
 import time
 
+import ssl
 import requests
 import websocket
 
@@ -221,10 +222,20 @@ class SymphonyGeothermal(object):
                 raise WFError()
 
     def _login_ws(self):
-        self.ws = websocket.create_connection(self.ws_url, timeout=TIMEOUT)
-        login = {"cmd": "login", "tid": self.tid,
-                 "source": "consumer dashboard",
-                 "sessionid": self.sessionid}
+        # The following is needed to allow legacy negotiation because
+        # WF is kind of slow in updating infrastructure
+        sslopt = {}
+        ctx = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        ctx.options |= 0x4  # OP_LEGACY_SERVER_CONNECT
+        sslopt.update({'context': ctx})
+        
+        self.ws = websocket.create_connection(self.ws_url, timeout=TIMEOUT, sslopt=sslopt)
+        login = {
+            "cmd": "login",
+            "tid": self.tid,
+            "source": "consumer dashboard",
+            "sessionid": self.sessionid,
+        }
         self.ws.send(json.dumps(login))
         # TODO(sdague): we should probably check the response, but
         # it's not clear anything is useful in it.
